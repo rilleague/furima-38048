7eaa155d34e2d2917cb4a412c965b0323a8472ae
rilleague <aarima2626@gmail.com>
2022-06-16 14:09:08 +0900

    【タスク４/商品出品機能 ※修正】出品機能実装　及び　モデル単体テスト実施

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,7 +17,6 @@ class Item < ApplicationRecord
     validates :shipping_cost_id, numericality: { other_than: 1 }
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :shipping_day_id, numericality: { other_than: 1 }
-    validates :selling_price, format: { with: /\A[0-9]+\z/ },
-                              numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :selling_price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 } 
   end
 end
