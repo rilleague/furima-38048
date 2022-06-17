dba1fc62324d9bbcd8d7ffc71dbfdcd1e2f3f367
rilleague <aarima2626@gmail.com>
2022-06-17 14:26:09 +0900

    【タスク8/商品削除機能】商品の情報を削除する機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :destory]
   before_action :set_item, only:[:show, :edit, :update]
   before_action :move_to_index, only: :edit
 
@@ -32,6 +32,12 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
   end
 
   private
